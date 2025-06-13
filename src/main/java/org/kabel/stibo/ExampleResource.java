ca86ac56a46da4ede61bdeaa4d5b05fcc5e9efd3
Dragonfly1895 <torben.kabel@gmail.com>
2025-06-13 09:22:12 +0200

    First change

--- src/main/java/org/kabel/stibo/ExampleResource.java
+++ src/main/java/org/kabel/stibo/ExampleResource.java
@@ -11,6 +11,18 @@ public class ExampleResource {
   @GET
   @Produces(MediaType.TEXT_PLAIN)
   public String hello() {
-    return "Hello RESTEasy";
+    return "Hello RESTEasy2";
   }
 }
+
+/*
+Successfully created project 'stibocase' on GitHub, but initial commit failed:
+
+Author identity unknown *** Please tell me who you are.
+Run
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name" to set your account's default identity.
+Omit --global to set the identity only in this repository. unable to auto-detect email address (got 'Torben@LUZERN.(none)')
+
+
+ */
